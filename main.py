35b41b1bed6d53a3e0133d3c9d3f6b7361254109
Cheekybantr <lishowyi@gmail.com>
2025-07-29 19:25:55 +1000

    updated usage: python3 main.py <path_to_book>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count
 from stats import letter_count
 from stats import sorted_letter_count
@@ -9,9 +10,12 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    num_words = word_count(get_book_text("books/frankenstein.txt"))
-    num_letters = letter_count(get_book_text("books/frankenstein.txt"))
-    num_letters_sorted = sorted_letter_count(letter_count(get_book_text("books/frankenstein.txt")))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    num_words = word_count(get_book_text(sys.argv[1]))
+    num_letters = letter_count(get_book_text(sys.argv[1]))
+    num_letters_sorted = sorted_letter_count(letter_count(get_book_text(sys.argv[1])))
     print(f"Found {num_words} total words")
     for each_letter in num_letters_sorted:
         print(f"{each_letter["char"]}: {each_letter["num"]}")
